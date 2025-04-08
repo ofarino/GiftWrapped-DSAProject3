a27e862e574706124ae72df8a4838b7c47417488
bcsanadi <bcsanadi01@gmail.com>
2025-04-08 16:04:23 -0400

    Added the struct Node and updated class Graph in graph.h

--- Graph.h
+++ Graph.h
@@ -4,10 +4,26 @@
 
 #ifndef GRAPH_H
 #define GRAPH_H
+#pragma once
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <string>
+using namespace std;
 
 
+struct Node {
+    string label;
+    vector<Node*> neighbors;
+
+};
 
 class Graph {
+    map<string, Node> graph;
+    void addNode(const string& label);
+    void addEdge(const string& from, const string& to);
+    vector<string> traverse(const vector<string>& userInput);
 
 };
 
